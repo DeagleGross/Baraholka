5ccbb82d5944865dd6a4e1916a3bd52a6e036f71
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-12-18 14:31:38 +0100

    skips

--- Benchmarks/Benchmarks/StackAllocsSizes.cs
+++ Benchmarks/Benchmarks/StackAllocsSizes.cs
@@ -3,17 +3,21 @@ using Microsoft.AspNetCore.DataProtection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Benchmarks
 {
     /*
-     | Method    | Mean     | Error     | StdDev    | Allocated |
-     |---------- |---------:|----------:|----------:|----------:|
-     | OneBig    | 5.819 ns | 0.0585 ns | 0.0547 ns |         - |
-     | TwoSmall  | 5.199 ns | 0.0701 ns | 0.0655 ns |         - |
-     | FourMicro | 6.102 ns | 0.0222 ns | 0.0197 ns |         - |
+        | Method            | Mean       | Error     | StdDev    | Allocated |
+        |------------------ |-----------:|----------:|----------:|----------:|
+        | OneBig            |  6.2626 ns | 0.0571 ns | 0.0506 ns |         - |
+        | OneBigWithSkip    |  0.3302 ns | 0.0116 ns | 0.0108 ns |         - |
+        | TwoSmall          | 11.6515 ns | 0.2354 ns | 0.2418 ns |         - |
+        | TwoSmallWithSkip  |  0.6846 ns | 0.0089 ns | 0.0083 ns |         - |
+        | FourMicro         |  5.3126 ns | 0.0318 ns | 0.0282 ns |         - |
+        | FourMicroWithSkip |  1.6459 ns | 0.0437 ns | 0.0409 ns |         - |
      */
 
     [MemoryDiagnoser]
@@ -21,6 +25,13 @@ namespace Benchmarks
     {
         [Benchmark]
         public int OneBig()
+        {
+            var arr = stackalloc byte[1024];
+            return arr[1];
+        }
+
+        [Benchmark, SkipLocalsInit]
+        public int OneBigWithSkip()
         {
             var arr = stackalloc byte[1024];
             return arr[1];
@@ -35,8 +46,28 @@ namespace Benchmarks
             return arr1[1] + arr2[1];
         }
 
+        [Benchmark, SkipLocalsInit]
+        public int TwoSmallWithSkip()
+        {
+            var arr1 = stackalloc byte[512];
+            var arr2 = stackalloc byte[512];
+
+            return arr1[1] + arr2[1];
+        }
+
         [Benchmark]
         public int FourMicro()
+        {
+            var arr1 = stackalloc byte[256];
+            var arr2 = stackalloc byte[256];
+            var arr3 = stackalloc byte[256];
+            var arr4 = stackalloc byte[256];
+
+            return arr1[1] + arr2[1] + arr3[1] + arr4[1];
+        }
+
+        [Benchmark, SkipLocalsInit]
+        public int FourMicroWithSkip()
         {
             var arr1 = stackalloc byte[256];
             var arr2 = stackalloc byte[256];
