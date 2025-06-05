809ca64bbf66c2f2193c864829b896aa45002aca
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-06-05 14:06:21 +0200

    test

---
+++ Orleans/MemoryOrleans/Models/DataGrain.cs
@@ -0,0 +1,24 @@
+﻿namespace MemoryOrleans.Models
+{
+    public interface IDataGrain : IGrainWithStringKey
+    {
+        Task SetData(byte[] data);
+        Task<long> GetDataSize();
+    }
+
+    public class DataGrain : Grain, IDataGrain
+    {
+        private byte[]? _data;
+
+        public Task SetData(byte[] data)
+        {
+            _data = data;
+            return Task.CompletedTask;
+        }
+
+        public Task<long> GetDataSize()
+        {
+            return Task.FromResult(_data?.LongLength ?? 0);
+        }
+    }
+}
---
+++ Orleans/MemoryOrleans/Program.cs
@@ -0,0 +1,46 @@
+using MemoryOrleans.Models;
+using Orleans.Configuration;
+using Orleans.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseOrleans(static siloBuilder =>
+{
+    siloBuilder.UseLocalhostClustering();
+    siloBuilder.AddMemoryGrainStorage("urls");
+
+    siloBuilder.Configure<GrainCollectionOptions>(options =>
+    {
+        options.MemoryBasedOptions = new MemoryBasedGrainCollectionOptions
+        {
+            MemoryLoadValidationQuantum = TimeSpan.FromSeconds(3),
+            HeapMemoryThresholdMb = 120,
+            TargetHeapMemoryMb = 100,
+            ThresholdMode = MemoryThresholdMode.AbsoluteMb
+        };
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapGet("/", () => "hello world");
+
+app.MapPost("/stress/{count}", async (IGrainFactory grains, int count = 100, int dataSizeKb = 512) =>
+{
+    var tasks = new List<Task>();
+    var data = new byte[dataSizeKb * 1024];
+    Random.Shared.NextBytes(data);
+
+    for (int i = 0; i < count; i++)
+    {
+        var grain = grains.GetGrain<IDataGrain>($"grain-{Guid.NewGuid()}");
+        tasks.Add(grain.SetData(data));
+    }
+
+    await Task.WhenAll(tasks);
+    return Results.Ok($"Created {count} grains, each with {dataSizeKb} KB of data.");
+});
+
+app.Run();
