3c19ab540debe3eaeaeb0c10b925634611893b47
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-07-18 17:02:30 +0200

    ai

--- AspireDF/AspireDF.ApiService/Program.cs
+++ AspireDF/AspireDF.ApiService/Program.cs
@@ -1,6 +1,7 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Queues;
-using Microsoft.Extensions.Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.AI;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,84 +9,73 @@ var builder = WebApplication.CreateBuilder(args);
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 
-builder.AddAzureBlobServiceClient("blobs");
-builder.AddAzureQueueServiceClient("queues");
+builder.AddAzureChatCompletionsClient("chat")
+       .AddChatClient();
+
+// for azure storage setup in AppHost
+//builder.AddAzureBlobServiceClient("blobs");
+//builder.AddAzureQueueServiceClient("queues");
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.MapGet("/", () => "Welcome to the AspireDF API Service!");
 
-app.MapGet("/blobs", (BlobServiceClient client) =>
+#region Azure AI Foundry
+
+app.MapGet("/chat", async ([FromServices] IChatClient chatClient) =>
 {
-    var res = new List<string>();
-
-    var containers = client.GetBlobContainers();
-    foreach (var container in containers)
-    {
-        res.Add(JsonSerializer.Serialize(container, new JsonSerializerOptions() { WriteIndented = true }));
-    }
-
-    return res;
+    var response = await chatClient.GetResponseAsync(new ChatMessage(ChatRole.User, content: "hello azure ai foundry!"));
+    return response.RawRepresentation;
 });
 
-app.MapGet("/blobs/{id}", (BlobServiceClient client, string id) =>
-{
-    return client.CreateBlobContainer(id);
-});
+#endregion
 
-app.MapGet("/queues", (QueueServiceClient client) =>
-{
-    var res = new List<string>();
+//#region Azure Storage
 
-    var queues = client.GetQueues();
-    foreach (var queue in queues)
-    {
-        res.Add(JsonSerializer.Serialize(queue, new JsonSerializerOptions() { WriteIndented = true }));
-    }
+//app.MapGet("/blobs", (BlobServiceClient client) =>
+//{
+//    var res = new List<string>();
 
-    return res;
-});
+//    var containers = client.GetBlobContainers();
+//    foreach (var container in containers)
+//    {
+//        res.Add(JsonSerializer.Serialize(container, new JsonSerializerOptions() { WriteIndented = true }));
+//    }
 
-app.MapGet("/queues/{id}", (QueueServiceClient client, string id) =>
-{
-    return client.CreateQueue(id);
-});
+//    return res;
+//});
 
+//app.MapGet("/blobs/{id}", (BlobServiceClient client, string id) =>
+//{
+//    return client.CreateBlobContainer(id);
+//});
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+//app.MapGet("/queues", (QueueServiceClient client) =>
+//{
+//    var res = new List<string>();
+
+//    var queues = client.GetQueues();
+//    foreach (var queue in queues)
+//    {
+//        res.Add(JsonSerializer.Serialize(queue, new JsonSerializerOptions() { WriteIndented = true }));
+//    }
+
+//    return res;
+//});
+
+//app.MapGet("/queues/{id}", (QueueServiceClient client, string id) =>
+//{
+//    return client.CreateQueue(id);
+//});
+
+//#endregion
 
 app.MapDefaultEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- AspireDF/AspireDF.AppHost/Program.cs
+++ AspireDF/AspireDF.AppHost/Program.cs
@@ -1,27 +1,55 @@
-// requirements:
-// run azurite for local emulator
-
 using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var storage = builder.AddAzureStorage("eastus");
-
-
-var blobs = storage.AddBlobService("blobs");
-var blobTest = storage.AddBlobContainer("testcont", "testcont");
-
-var queues = storage.AddQueueService("queues");
-var eventsQ = storage.AddQueue("events", "events");
-
-var apiService = builder.AddProject<Projects.AspireDF_ApiService>("apiservice")
-    .WithReference(blobs).WithReference(queues)
-    .WaitFor(blobs)
-    .WaitFor(queues);
-
-//builder.AddProject<Projects.AspireDF_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(apiService)
-//    .WaitFor(apiService);
+// builder = AzureStorageSetup(builder);
+// builder = AzureAiFoundryLocalSetup(builder);
+builder = AzureAiFoundrySetup(builder);
 
 builder.Build().Run();
+
+IDistributedApplicationBuilder AzureAiFoundryLocalSetup(IDistributedApplicationBuilder builder)
+{
+    var foundry = builder.AddAzureAIFoundry("foundry").RunAsFoundryLocal();
+    // var chat = foundry.AddDeployment("chat", "qwen2.5-0.5b", "1", "Microsoft");
+    var chat = foundry.AddDeployment("chat", "DeepSeek-R1-0528", "1", "Microsoft");
+
+    var apiService = builder
+        .AddProject<Projects.AspireDF_ApiService>("apiservice")
+        .WithReference(chat)
+        .WaitFor(chat);
+
+    return builder;
+}
+IDistributedApplicationBuilder AzureAiFoundrySetup(IDistributedApplicationBuilder builder)
+{
+    var foundry = builder.AddAzureAIFoundry("foundry");
+    var chat = foundry.AddDeployment("chat", "qwen2.5-0.5b", "1", "Microsoft");
+
+    var apiService = builder
+        .AddProject<Projects.AspireDF_ApiService>("apiservice")
+        .WithReference(chat)
+        .WaitFor(chat);
+
+    return builder;
+}
+
+
+IDistributedApplicationBuilder AzureStorageSetup(IDistributedApplicationBuilder builder)
+{
+    var storage = builder.AddAzureStorage("eastus");
+
+
+    var blobs = storage.AddBlobService("blobs");
+    var blobTest = storage.AddBlobContainer("testcont", "testcont");
+
+    var queues = storage.AddQueueService("queues");
+    var eventsQ = storage.AddQueue("events", "events");
+
+    var apiService = builder.AddProject<Projects.AspireDF_ApiService>("apiservice")
+        .WithReference(blobs).WithReference(queues)
+        .WaitFor(blobs)
+        .WaitFor(queues);
+
+    return builder;
+}
